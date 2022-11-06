a8775a745343a09de701cb9d34bda540ce06ce78
PremKumbhar <kumbharprem7738@gmail.com>
2022-11-06 19:39:32 +0530

    [Prem]:Added find frequency of words in a large paragraph

--- src/com/bridgelabz/MainHashTable.java
+++ src/com/bridgelabz/MainHashTable.java
@@ -1,19 +1,20 @@
 package com.bridgelabz;
 
 public class MainHashTable {
-	 public static void main(String[] args) {
-	        MyHashTable<String, Integer> hashTable1 = new MyHashTable<>(10);
-	        String paragraph = "To be or not to be";
-	        String[] array = paragraph.split(" ");
-	        for (String str : array) {
-	            Integer frequencyValue = hashTable1.findFrequency(str);
-	            if (frequencyValue == null) {
-	                frequencyValue = 1;
-	            } else {
-	                frequencyValue += 1;
-	            }
-	            hashTable1.add(str, frequencyValue);
+	public static void main(String[] args) {
+		MyHashTable<String, Integer> hashTable1 = new MyHashTable<>(10);
+	    String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+	    //String[] array = paragraph.split(" ");
+	    String[] array = paragraph.split(" ");
+	    for (String str:array) {
+	        Integer frequencyValue = hashTable1.findFrequency(str);
+	        if (frequencyValue==null){
+	            frequencyValue=1;
+	        }else {
+	            frequencyValue+=1;
 	        }
-	        System.out.println(hashTable1);
+	        hashTable1.add(str,frequencyValue);
 	    }
+	    System.out.println(hashTable1);
+	}
 }
--- src/com/bridgelabz/MyHashTable.java
+++ src/com/bridgelabz/MyHashTable.java
@@ -65,4 +65,4 @@ public class MyHashTable<K,V> {
         return  ""+ myBucketArray ;
 
     }
-	}
+}
